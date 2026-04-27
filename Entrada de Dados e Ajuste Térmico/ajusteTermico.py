e237a2ea19091af37b8e69852a6cadc71a7d0ac2
elipec08 <felipe.ec4@puccampinas.edu.br>
2026-04-27 10:50:30 -0300

    codigo inteiro e função colocada

--- Entrada de Dados e Ajuste Térmico/ajusteTermico.py
+++ Entrada de Dados e Ajuste Térmico/ajusteTermico.py
@@ -1,6 +1,16 @@
-quant_leituras=int(input('Digite quantas leituras vai fazer no seu turno: '))
-
-cont=0
-
-while cont<=(quant_leituras+1):
-    leitura=int(input('Leitura na pressão hidrodinâmica do duto: '))
+def leitura():
+    quant_leituras=int(input('Digite quantas leituras vai fazer no seu turno: '))
+    
+    cont=0
+    
+    leituras=[]
+    
+    while cont<=(quant_leituras-1):
+        leitura=int(input('Leitura da pressão hidrodinâmica do duto: '))
+        cont+=1
+        if leitura>150:
+            leitura=leitura+(leitura*0.08)
+        else:
+            leitura=leitura-(leitura*0.04)
+        leituras.append(leitura)
+    print(leituras)
