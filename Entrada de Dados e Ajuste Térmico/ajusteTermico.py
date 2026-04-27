31e813e03fe837da58b9b3a09b97b819773fb129
Felipe Evangelista Cruz <felipe.ec4@puccampinas.edu.br>
2026-04-27 10:48:14 -0300

    entrada das leituras e acrescimos

--- Entrada de Dados e Ajuste Térmico/ajusteTermico.py
+++ Entrada de Dados e Ajuste Térmico/ajusteTermico.py
@@ -1,1 +1,6 @@
+quant_leituras=int(input('Digite quantas leituras vai fazer no seu turno: '))
 
+cont=0
+
+while cont<=(quant_leituras+1):
+    leitura=int(input('Leitura na pressão hidrodinâmica do duto: '))
